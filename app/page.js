3949caead281d96fb225f870abc3246c52ec5290
learnaighl-source <learnaighl@gmail.com>
2025-12-01 02:50:56 +0500

    hello

--- app/page.js
+++ app/page.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 const page = () => {
-  const userName = "John Doe"; // Replace with actual user name
-
-  return (
-    <div>
-      <h1>Welcome, {userName}!</h1>
-      <div>page</div>
-    </div>
-  );
+  return <div>page</div>;
 };
 
 export default page;
