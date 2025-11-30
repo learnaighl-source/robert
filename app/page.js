8445b720d9ba8226e0c41b2cada96ba93ae2682b
learnaighl-source <learnaighl@gmail.com>
2025-12-01 02:58:19 +0500

    hello

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <div className="flex justify-end p-4">
+        <h1 className="text-2xl font-bold">Free Slots</h1>
+      </div>
+    </div>
+  );
 };
 
 export default page;
